abc84e252d15a35ee185e31ccd195d0e300011bb
Sil silas <silikhesilas@gmail.com>
2019-11-08 01:02:14 +0300

    Completed module

--- js/sight.js
+++ js/sight.js
@@ -21,9 +21,9 @@ class SVGElement {
 
 class Sight {
     constructor (selector, width, height){
-        this.svg = new SVGAElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
+        this.svg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
     }
     draw(type, attrs){
-        return new SVGAElement(type).attr(attrs).append(this.svg)
+        return new SVGElement(type).attr(attrs).append(this.svg)
     }
 }
